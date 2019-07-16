8ee34e4265fd5ee45f4b5a582a754f6c1e075b81
Marek <jonas.wu@waystorm.com>
2019-07-16 13:58:38 +0800

    Updates

---
+++ precache-manifest.0ab2a7040214836b75aeb65554c673ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba73d4c0b98844f7bad5f76d35849b6",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/robotfriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "5df4e27fabf9e14f4b09",
+    "url": "/robotfriends/static/css/main.2f2e46cf.chunk.css"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/robotfriends/static/js/2.abd0d98f.chunk.js"
+  },
+  {
+    "revision": "5df4e27fabf9e14f4b09",
+    "url": "/robotfriends/static/js/main.b5f63b5d.chunk.js"
+  },
+  {
+    "revision": "aebb4e0e6c2039467182",
+    "url": "/robotfriends/static/js/runtime~main.6fe61ee8.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
