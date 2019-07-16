d3a6a5863302197a695336cb3f73fbd35cd3636a
Marek <jonas.wu@waystorm.com>
2019-07-16 13:47:34 +0800

    src

---
+++ build/precache-manifest.fe4b39cf868a444272bab1311746f5a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108d1dced8c1a35cb01a0c205f51c726",
+    "url": "/robotfriends/src/index.html/index.html"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/robotfriends/src/index.html/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "15f703e9a16dbbddc5a7",
+    "url": "/robotfriends/src/index.html/static/css/main.b9afdc0d.chunk.css"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/robotfriends/src/index.html/static/js/2.abd0d98f.chunk.js"
+  },
+  {
+    "revision": "15f703e9a16dbbddc5a7",
+    "url": "/robotfriends/src/index.html/static/js/main.fb65ddb6.chunk.js"
+  },
+  {
+    "revision": "b68150418c967dc7ddce",
+    "url": "/robotfriends/src/index.html/static/js/runtime~main.ece7cbbd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/src/index.html/static/media/SEGA.12a3db8b.woff"
+  }
+]);
